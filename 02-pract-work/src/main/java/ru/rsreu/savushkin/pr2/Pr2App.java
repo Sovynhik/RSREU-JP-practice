49bfb6c9586c69103efdb435477724b275dcb9fc
Daniel Savushkin <savushckindaniil@yandex.ru>
2026-04-11 06:17:12 +0300

    feat(pr-02): complete pr #2

---
+++ 02-pract-work/src/main/java/ru/rsreu/savushkin/pr2/Pr2App.java
@@ -0,0 +1,13 @@
+package ru.rsreu.savushkin.pr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr2App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr2App.class, args);
+	}
+
+}
